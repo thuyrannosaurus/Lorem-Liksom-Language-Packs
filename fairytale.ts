49f123e6c27a2bcec9b9170c386409aa76763a28
Thuy Gia Nguyen <73501955+thuyrannosaurus@users.noreply.github.com>
2025-02-25 12:27:51 +0100

    Update fairytale.ts

--- fairytale.ts
+++ fairytale.ts
@@ -1,9 +1,15 @@
 import type { TextCollection } from '../types';
 
 export const fairytaleTexts: TextCollection = {
-
-
-
-  
-      ]
+    short: [
+        // 100 KORTE TEKSTER (30–80 tegn, barnlig begeistring, 1 setning)
+        
+    ],
+    medium: [
+        // 100 MELLOMLANGE TEKSTER (2–3 setninger, 150–250 tegn, barnlig engasjement)
+        
+    ],
+    long: [
+        // 50 LANGE TEKSTER (550–900 tegn, flere setninger, barnlig fortellerglede)
+        
 };
